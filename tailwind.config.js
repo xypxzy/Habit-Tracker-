48595aed860d1e14261e4d3fe677524cebb8c739
xypxzy <xypxzy@gmail.com>
2023-02-07 20:49:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        swatch_1: '#e9eff8',
+        swatch_2: '#a5b8d8',
+        swatch_3: '#0229f8',
+        swatch_4: '#929cac',
+        swatch_5: '#0a72f2',
+        swatch_6: '#484848',
+        swatch_7: '#7878b7',
+      },
+    },
+  },
+  plugins: [],
+};
